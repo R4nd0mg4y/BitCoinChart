5b40bdf8d988f945df31630f965a24c6f6fc7e02
R4nd0mg4y <tuandoviet2002@gmail.com>
2025-03-01 11:39:54 +0700

    add toolkit

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -7,10 +7,12 @@ import {
   UTCTimestamp,
   HistogramSeries,
   CandlestickData,
+  MouseEventParams,
 } from "lightweight-charts";
 import { GetCandles, GetLiveCandle } from "@/utils/fetchApi";
 import { ISeriesApi } from "lightweight-charts";
 import { Button } from "primereact/button";
+import { unixToDate } from "@/utils/fetchApi";
 
 type ChartProps = {
   darkMode?: boolean;
@@ -40,11 +42,14 @@ const Chart = ({ darkMode = false, cryptoName, timeFrame }: ChartProps) => {
   >([]);
   const wsRef = useRef<WebSocket | null>(null);
   const chartContainerRef = useRef<HTMLDivElement | null>(null);
+  const toolTipRef = useRef<HTMLDivElement | null>(null);
   const [candleSeries, setCandleSeries] =
     useState<ISeriesApi<"Candlestick"> | null>(null);
   const [volumeSeries, setVolumeSeries] =
     useState<ISeriesApi<"Histogram"> | null>(null);
   const [price, setPrice] = useState<Price | null>(null);
+  const candleSeriesRef = useRef<ISeriesApi<"Candlestick"> | null>(null);
+
   useEffect(() => {
     const fetchData = async () => {
       const newData = await GetCandles(timeFrame, cryptoName);
@@ -82,7 +87,12 @@ const Chart = ({ darkMode = false, cryptoName, timeFrame }: ChartProps) => {
         textColor: darkMode ? "white" : "black",
       },
       width: chartContainerRef.current.clientWidth,
-      height: chartContainerRef.current.clientHeight || 600, 
+      height: chartContainerRef.current.clientHeight || 600,
+      rightPriceScale: {
+        visible: true,
+        borderColor: darkMode ? "#444" : "#ddd",
+        ticksVisible: true,
+      },
     });
 
     const timeScale = chart.timeScale();
@@ -90,7 +100,7 @@ const Chart = ({ darkMode = false, cryptoName, timeFrame }: ChartProps) => {
       timeVisible: true,
       secondsVisible: true,
     });
-    
+
     const newCandleSeries = chart.addSeries(CandlestickSeries, {
       priceScaleId: "candles",
     });
@@ -110,6 +120,92 @@ const Chart = ({ darkMode = false, cryptoName, timeFrame }: ChartProps) => {
         top: 0.7,
         bottom: 0,
       },
+    });
+    const toolTipWidth = 80;
+    const toolTipHeight = 80;
+    const toolTipMargin = 15;
+
+    const toolTip = document.createElement("div");
+    toolTip.style.width = "150px";
+    toolTip.style.height = "130px";
+    toolTip.style.position = "absolute";
+    toolTip.style.display = "none";
+    toolTip.style.padding = "8px";
+    toolTip.style.boxSizing = "border-box";
+    toolTip.style.fontSize = "12px";
+    toolTip.style.textAlign = "left";
+    toolTip.style.zIndex = "1000";
+    toolTip.style.top = "12px";
+    toolTip.style.left = "12px";
+    toolTip.style.pointerEvents = "none";
+    toolTip.style.border = "1px solid";
+    toolTip.style.borderRadius = "2px";
+    toolTip.style.fontFamily =
+      "-apple-system, BlinkMacSystemFont, 'Trebuchet MS', Roboto, Ubuntu, sans-serif";
+    toolTip.style.background = "white";
+    toolTip.style.color = "black";
+    toolTip.style.borderColor = "#2962FF";
+
+    chartContainerRef.current.appendChild(toolTip);
+    chart.subscribeCrosshairMove((param) => {
+      if (
+        param.point === undefined ||
+        !param.time ||
+        param.point.x < 0 ||
+        !chartContainerRef.current ||
+        param.point.x > chartContainerRef.current.clientWidth ||
+        param.point.y < 0 ||
+        param.point.y > chartContainerRef.current.clientHeight
+      ) {
+        toolTip.style.display = "none";
+      } else {
+        // time will be in the same format that we supplied to setData.
+        // thus it will be YYYY-MM-DD
+        // const dateStr = param.time;
+        toolTip.style.display = "block";
+        const data = param.seriesData.get(newCandleSeries);
+        const { open, high, low, close, time } = data as {
+          open: number;
+          high: number;
+          low: number;
+          close: number;
+          time: UTCTimestamp
+        };
+        
+        // Format lại tooltip
+        toolTip.innerHTML = `
+          <div style="font-size: 14px; margin: 4px 0px; color: black">Open: ${open.toFixed(2)}</div>
+          <div style="font-size: 14px; color: black">High: ${high.toFixed(2)}</div>
+          <div style="font-size: 14px; color: black">Low: ${low.toFixed(2)}</div>
+          <div style="font-size: 14px; color: black">Close: ${close.toFixed(2)}</div>
+          <div style="color: black; margin-top: 4px;">${unixToDate(time*1000)}</div>
+        `;
+
+        const coordinate = newCandleSeries.priceToCoordinate((high + low) / 2);
+        let shiftedCoordinate = param.point.x - 50;
+        if (coordinate === null) {
+          return;
+        }
+        shiftedCoordinate = Math.max(
+          0,
+          Math.min(
+            chartContainerRef.current.clientWidth - toolTipWidth,
+            shiftedCoordinate
+          )
+        );
+        const coordinateY =
+          coordinate - toolTipHeight - toolTipMargin > 0
+            ? coordinate - toolTipHeight - toolTipMargin
+            : Math.max(
+                0,
+                Math.min(
+                  chartContainerRef.current.clientHeight - toolTipHeight - toolTipMargin,
+                  coordinate + toolTipMargin
+                )
+              );
+        toolTip.style.left = shiftedCoordinate + "px";
+        toolTip.style.top = coordinateY + "px";
+      }
     });
 
     newCandleSeries.setData(chartData);
@@ -157,7 +253,6 @@ const Chart = ({ darkMode = false, cryptoName, timeFrame }: ChartProps) => {
           value: parseFloat(candle.v),
         };
 
-
         candleSeries?.update(updatedCandle);
         volumeSeries?.update(updatedHistogram);
       }
@@ -171,24 +266,26 @@ const Chart = ({ darkMode = false, cryptoName, timeFrame }: ChartProps) => {
 
   return (
     <div className="w-full h-[300px] sm:h-[400px]">
-      <div ref={chartContainerRef} className="w-full h-full" />
-        <div className="flex flex-row  items-center my-5">
-          <Button
-            label="Lấy giá Bitcoin"
-            onClick={() => {
-              const candles =
-                candleSeries?.data() as CandlestickData<UTCTimestamp>[];
-              setPrice({
-                currentPrice: candles?.[candles.length - 1]?.close ?? 0,
-                prevPrice: candles?.[candles.length - 2]?.close ?? 0,
-              });
-            }}
-          />
-          <div className="mx-5">
-            <p>Giá hiện tại: ${price?.currentPrice}</p>
-            <p>Giá cách đây {timeFrame}: ${price?.prevPrice}</p>
-          </div>
+      <div ref={chartContainerRef} className=" w-full h-full" id="container" />
+      <div className="flex flex-row  items-center my-5">
+        <Button
+          label="Lấy giá Bitcoin"
+          onClick={() => {
+            const candles =
+              candleSeries?.data() as CandlestickData<UTCTimestamp>[];
+            setPrice({
+              currentPrice: candles?.[candles.length - 1]?.close ?? 0,
+              prevPrice: candles?.[candles.length - 2]?.close ?? 0,
+            });
+          }}
+        />
+        <div className="mx-5">
+          <p>Giá hiện tại: ${price?.currentPrice}</p>
+          <p>
+            Giá cách đây {timeFrame}: ${price?.prevPrice}
+          </p>
         </div>
+      </div>
     </div>
   );
 };
--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -6,7 +6,7 @@ const DarkModeToggle = () => {
   return (
     <button
       onClick={toggleDarkMode}
-      className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors"
+      className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors m-2"
     >
       {isDarkMode ? "🌙 Tắt chế độ tối" : "☀️ Bật chế độ tối"}
     </button>
